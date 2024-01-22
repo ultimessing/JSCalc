a79024f412035e68f5ad11d5aec2c575dbb18752
ultimessing <ovrik120@gmail.com>
2024-01-22 22:32:36 +0200

    Add functionality for all buttons other than the dot

--- script.js
+++ script.js
@@ -2,24 +2,59 @@ let firstNumber = '';
 let secondNumber = '';
 let firstNumberDone = false;
 let operator = '';
+const numButtons = document.querySelectorAll('[data-number]');
+const dotButton = document.querySelector('#dot');
+const opButtons = document.querySelectorAll(`[data-operator]`);
+const equalsButton = document.querySelector('#equals');
+const displayStr = document.querySelector(`#displayedStr`);
 
+function updateDisplay() {
+    displayStr.textContent = `${firstNumber} ${operator} ${secondNumber}`;
+}
+
+function resultDisplay(num) {
+    firstNumber = num;
+    secondNumber = '';
+    operator = '';
+    firstNumberDone = false;
+    updateDisplay();
+}
 
 function appendNum(num) {
-if (firstNumberDone) {
-    secondNumber += `${num}`;
-}
-    firstNumber += `${num}`;
+    if (firstNumberDone) {
+        secondNumber += `${num}`;
+    } else {
+        firstNumber += `${num}`;
+    }
+    updateDisplay();
 }
 
-const numButtons = document.querySelectorAll('[data-number]');
 numButtons.forEach(x => x.addEventListener(('click'), () => appendNum(x.textContent)));
-const dotButton = document.querySelector('#dot');
+
+opButtons.forEach(x => x.addEventListener(('click'), () => {
+    operator = x.textContent;
+    firstNumberDone = true;
+    updateDisplay();
+}
+));
 
 
-const opButtons = document.querySelectorAll(`[data-operator]`);
-opButtons.forEach(x => x.addEventListener(('click'), () => operator = x.textContent));
-const plusButton = document.querySelector('#plus');
-const minusButton = document.querySelector('#minus');
-const multiplyButton = document.querySelector('#multiply');
-const divideButton = document.querySelector('#divide');
-const equalsButton = document.querySelector('#equals');
+equalsButton.addEventListener(('click'), () => {
+    let firstNum = parseFloat(firstNumber);
+    let secondNum = parseFloat(secondNumber);
+    switch (operator) {
+        case '+':
+            resultDisplay(firstNum + secondNum);
+            break;
+        case '-':
+            resultDisplay(firstNum - secondNum);
+            break;
+        case '*':
+            resultDisplay(firstNum * secondNum);   
+            break;
+        case '/':
+            if (secondNum === 0) return "Can't divide by zero"
+            resultDisplay(Math.round((firstNum / secondNum) * 100) / 100);
+            break;
+    }
+})
